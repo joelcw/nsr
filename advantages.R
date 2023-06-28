f544a3404aac019b4993afa9913593d378f60b3d
Joel <joel.wallenberg@gmail.com>
2023-06-28 08:55:50 +0100

    fixing queries and output

--- advantages.R
+++ advantages.R
@@ -1,50 +1,144 @@
 library(ggplot2)
 library(plyr)
 
-foo <- read.delim("~/constantentropy/outputs/infoTheoryTest.ymeb.cod.ooo",header=F,sep=":")
+foo <- read.delim("~/nsr/outputs/NSRenvirons.pceec.cod.fixed.ooo",header=F,sep=":")
 
 
 ####Give appropriate column names to the columns
-colnames(foo) <- c("OV","Clause","ObjType","SbjType","ID","Year")
+colnames(foo) <- c("Sbj","Verb","Adjacent","Agr","ID")
 
 
 ####Throw out all the codes that refer to tokens that are irrelevant for the study.
-objsbj.data <- subset(foo, OV != "z" & Clause != "z" & Year != "0" & Year != "" & Year != "na" & SbjType != "z" & SbjType != "" & ObjType != "z" & ObjType != "" & ID != "")
+foo.data <- subset(foo, Sbj != "z" & Verb != "z" & Adjacent != "z" & Sbj != "othersbj" & Agr != "z")
 
 library(gdata)
 
 ####Make sure R factor groups don't include factors for the irrelevant codes.
-objsbj.data <- droplevels(objsbj.data)
+foo.data <- droplevels(foo.data)
+
+foo.table <- table(foo.data$Sbj,foo.data$Adjacent)
+fooFreq.table <- foo.table/sum(foo.table)
 
 sg1Adj <- 4974
-sg2Adj <- 0
+sg2Adj <- (961+25)*(0.95) #961 vs 25 is Lisa's original adjacent vs non-adjacent split, but to just multiply by the freqs of sing vs plural would assume independence bet/ adjacency and number, which is wrong based on her sample. So I've added them together to re-estimate adjacent and non-adjacent from Lisa's sample-of-you freqs.
 sg3Adj <- 1555
 pl13Adj <- 637
-pl2Adj <- 961 - xxx
+pl2Adj <- (961+25)*(0.03)
 DPsgAdj <- 1810
 DPplAdj <- 264
 sg1Non <- 78
-sg2Non <- 0
+sg2Non <- (961+25)*(0.01)
 sg3Non <- 33
 pl13Non <- 25
-pl2Non <- 25 - xxx
+pl2Non <- (961+25)*(0.01)
 DPsgNon <- 1526
 DPplNon <- 193
+
+Total <- sg1Adj + sg2Adj + sg3Adj + pl13Adj + pl2Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPsgNon + DPplNon
+
+sg1Adj <- sg1Adj/Total
+sg2Adj <- sg2Adj/Total #961 vs 25 is Lisa's original adjacent vs non-adjacent split, but to just multiply by the freqs of sing vs plural would assume independence bet/ adjacency and number, which is wrong based on her sample. So I've added them together to re-estimate adjacent and non-adjacent from Lisa's sample-of-you freqs.
+sg3Adj <- sg3Adj/Total
+pl13Adj <- pl13Adj/Total
+pl2Adj <- pl2Adj/Total
+DPsgAdj <- DPsgAdj/Total
+DPplAdj <- DPplAdj/Total
+sg1Non <- sg1Non/Total
+sg2Non <- sg2Non/Total
+sg3Non <- sg3Non/Total
+pl13Non <- pl13Non/Total
+pl2Non <- pl2Non/Total
+DPsgNon <- DPsgNon/Total
+DPplNon <- DPplNon/Total
+
 
 #Threeway calculation assuming no t/d-deletion
 NSR <- (sg2Adj+sg2Non) + DPplAdj + sg1Non + pl13Non + pl2Non + DPplNon
 StE <- sg2Non + sg2Adj
 Zero <- sg2Adj+sg2Non + sg3Adj + DPsgAdj + sg3Non + DPsgNon
+
+NSR
+StE
+Zero
+
 #Threeway calculation assuming t/d-deletion
 NSR <- DPplAdj + sg1Non + pl13Non + pl2Non + DPplNon
 StE <- sg2Non + sg2Adj
 Zero <- sg2Adj+sg2Non + sg3Adj + DPsgAdj + sg3Non + DPsgNon
 
+NSR
+StE
+Zero
+
 #Pairwise calculation assuming no t/d-deletion
 NSRwrtStE <- (sg2Adj+sg2Non) + DPplAdj + sg1Non + pl13Non + pl2Non  + DPplNon
-NSRwrtZero <- sg2Adj+sg2Non + sg3Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPplNon
+NSRwrtZero <- sg2Adj+sg2Non + sg3Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPsgNon + DPplNon
 StEwrtNSR <- sg2Non + sg2Adj + DPplAdj +sg1Non + pl13Non + pl2Non  + DPplNon
 StEwrtZero <- sg2Non + sg2Adj + sg3Adj + DPsgAdj + sg3Non + DPsgNon
 ZeroWrtNSR <- sg2Adj + sg2Non + sg3Adj +DPsgAdj + DPplAdj + sg1Non + sg3Non + pl13Non + pl2Non +  DPsgNon + DPplNon
-ZeroWrtStE <- (sg2Non + sg2Adj) + sg3Adj + DPsgAdj + DPsgNon + sg3Non + DPsgNon 
-#Pairwise calculation assuming t/d-deletion; note that Zero can use td-deletion in modals
+ZeroWrtStE <- (sg2Non + sg2Adj) + sg3Adj + DPsgAdj + sg3Non + DPsgNon 
+
+"NSRwrtStE"
+NSRwrtStE
+"StEwrtNSR"
+StEwrtNSR
+"NSRwrtZero"
+NSRwrtZero
+"ZeroWrtNSR"
+ZeroWrtNSR
+"StEwrtZero"
+StEwrtZero
+"ZeroWrtStE"
+ZeroWrtStE
+
+#Pairwise calculation assuming t/d-deletion; note that StE can use td-deletion with 2sg in modals
+NSRwrtStE <-  DPplAdj + sg1Non + pl13Non + pl2Non  + DPplNon
+NSRwrtZero <- sg2Adj+sg2Non + sg3Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPsgNon + DPplNon
+StEwrtNSR <- sg2Non + sg2Adj + DPplAdj +sg1Non + pl13Non + pl2Non  + DPplNon
+StEwrtZero <- sg2Non + sg2Adj + sg3Adj + DPsgAdj + sg3Non + DPsgNon
+ZeroWrtNSR <- sg2Adj + sg2Non + sg3Adj +DPsgAdj + DPplAdj + sg1Non + sg3Non + pl13Non + pl2Non +  DPsgNon + DPplNon
+ZeroWrtStE <- (sg2Non + sg2Adj) + sg3Adj + DPsgAdj + sg3Non + DPsgNon 
+
+"NSRwrtStE"
+NSRwrtStE
+"StEwrtNSR"
+StEwrtNSR
+"NSRwrtZero"
+NSRwrtZero
+"ZeroWrtNSR"
+ZeroWrtNSR
+"StEwrtZero"
+StEwrtZero
+"ZeroWrtStE"
+ZeroWrtStE
+
+
+modal.table <- table(foo.data$Sbj,foo.data$Verb)
+
+#Advantage with modal and t/d, assuming modals are 40%
+
+sg2AdjV <- 0.6*sg2Adj
+sg2AdjMod <- 0.4*sg2Adj
+sg2NonV <- 0.6*sg2Non
+sg2NonMod <- 0.4*sg2Non
+
+#This may be an exaggeration, since shallt and willt aren't the same as wouldst, couldst, might, mayst, canst
+NSRwrtStE <-  DPplAdj + sg1Non + pl13Non + pl2Non  + DPplNon
+NSRwrtZero <- sg2AdjV+sg2NonV + sg3Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPsgNon + DPplNon
+StEwrtNSR <- sg2Non + sg2Adj + DPplAdj +sg1Non + pl13Non + pl2Non  + DPplNon
+StEwrtZero <- sg2Non + sg2Adj + sg3Adj + DPsgAdj + sg3Non + DPsgNon
+ZeroWrtNSR <- sg2AdjV + sg2NonV + sg3Adj +DPsgAdj + DPplAdj + sg1Non + sg3Non + pl13Non + pl2Non +  DPsgNon + DPplNon
+ZeroWrtStE <- sg2AdjV + sg2NonV + sg3Adj + DPsgAdj + sg3Non + DPsgNon 
+
+"NSRwrtStE"
+NSRwrtStE
+"StEwrtNSR"
+StEwrtNSR
+"NSRwrtZero"
+NSRwrtZero
+"ZeroWrtNSR"
+ZeroWrtNSR
+"StEwrtZero"
+StEwrtZero
+"ZeroWrtStE"
+ZeroWrtStE
---
+++ fixIdFormat.eng.py
@@ -0,0 +1,20 @@
+#16Nov2022
+#Script takes the output of CS extracting codes with printing token ids, and converts the id into a coding column for text; so it changes @ to : and removes token number
+#This one works for the PPCHE, *not* for IcePaHC
+
+import sys,string,re
+
+#Note that the second group below is made non-greedy because of some YCOE ids that have another comma in them later on...but check this behaviour 
+
+idregex = re.compile("(@)(.*?)(,.*)$")
+
+#function returning id in the right format 
+
+def textid(matchobj):
+    return (":%s" % matchobj.group(2))
+
+lines = sys.stdin.readlines()
+
+for line in lines:
+    new = idregex.sub(textid,line)
+    sys.stdout.write(new.lower())
