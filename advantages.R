b2fc11dd0b31961ca310bfddcd1775c4c8de598f
Joel <joel.wallenberg@gmail.com>
2023-07-03 14:24:55 +0100

    added talk

--- advantages.R
+++ advantages.R
@@ -19,20 +19,20 @@ foo.data <- droplevels(foo.data)
 foo.table <- table(foo.data$Sbj,foo.data$Adjacent)
 fooFreq.table <- foo.table/sum(foo.table)
 
-sg1Adj <- 4974
-sg2Adj <- (961+25)*(0.95) #961 vs 25 is Lisa's original adjacent vs non-adjacent split, but to just multiply by the freqs of sing vs plural would assume independence bet/ adjacency and number, which is wrong based on her sample. So I've added them together to re-estimate adjacent and non-adjacent from Lisa's sample-of-you freqs.
-sg3Adj <- 1555
-pl13Adj <- 637
-pl2Adj <- (961+25)*(0.03)
+sg1Adj <- 4986
+sg2Adj <- (966+20)*(0.95) #961 vs 25 is Lisa's original adjacent vs non-adjacent split, but to just multiply by the freqs of sing vs plural would assume independence bet/ adjacency and number, which is wrong based on her sample. So I've added them together to re-estimate adjacent and non-adjacent from Lisa's sample-of-you freqs.
+sg3Adj <- 1569
+pl13Adj <- 643
+pl2Adj <- (966+20)*(0.03)
 DPsgAdj <- 1810
-DPplAdj <- 264
-sg1Non <- 78
-sg2Non <- (961+25)*(0.01)
-sg3Non <- 33
-pl13Non <- 25
-pl2Non <- (961+25)*(0.01)
+DPplAdj <- 265
+sg1Non <- 66
+sg2Non <- (966+20)*(0.01)
+sg3Non <- 19
+pl13Non <- 19
+pl2Non <- (966+20)*(0.01)
 DPsgNon <- 1526
-DPplNon <- 193
+DPplNon <- 192
 
 Total <- sg1Adj + sg2Adj + sg3Adj + pl13Adj + pl2Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPsgNon + DPplNon
 
@@ -61,9 +61,9 @@ NSR
 StE
 Zero
 
-#Threeway calculation assuming t/d-deletion
+#Threeway calculation assuming t/d-deletion; retention = 0.84 in past tense from guy 1990, = 51% following s from guy and boberg 1997 
 NSR <- DPplAdj + sg1Non + pl13Non + pl2Non + DPplNon
-StE <- sg2Non + sg2Adj
+StE <- ((sg2Non + sg2Adj)*(0.84*0.51))
 Zero <- sg2Adj+sg2Non + sg3Adj + DPsgAdj + sg3Non + DPsgNon
 
 NSR
@@ -94,7 +94,7 @@ ZeroWrtStE
 #Pairwise calculation assuming t/d-deletion; note that StE can use td-deletion with 2sg in modals
 NSRwrtStE <-  DPplAdj + sg1Non + pl13Non + pl2Non  + DPplNon
 NSRwrtZero <- sg2Adj+sg2Non + sg3Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPsgNon + DPplNon
-StEwrtNSR <- sg2Non + sg2Adj + DPplAdj +sg1Non + pl13Non + pl2Non  + DPplNon
+StEwrtNSR <- ((sg2Non + sg2Adj)*(0.82*0.58)) + DPplAdj +sg1Non + pl13Non + pl2Non  + DPplNon
 StEwrtZero <- sg2Non + sg2Adj + sg3Adj + DPsgAdj + sg3Non + DPsgNon
 ZeroWrtNSR <- sg2Adj + sg2Non + sg3Adj +DPsgAdj + DPplAdj + sg1Non + sg3Non + pl13Non + pl2Non +  DPsgNon + DPplNon
 ZeroWrtStE <- (sg2Non + sg2Adj) + sg3Adj + DPsgAdj + sg3Non + DPsgNon 
@@ -125,7 +125,7 @@ sg2NonMod <- 0.4*sg2Non
 #This may be an exaggeration, since shallt and willt aren't the same as wouldst, couldst, might, mayst, canst
 NSRwrtStE <-  DPplAdj + sg1Non + pl13Non + pl2Non  + DPplNon
 NSRwrtZero <- sg2AdjV+sg2NonV + sg3Adj + DPsgAdj + DPplAdj + sg1Non + sg2Non + sg3Non + pl13Non + pl2Non + DPsgNon + DPplNon
-StEwrtNSR <- sg2Non + sg2Adj + DPplAdj +sg1Non + pl13Non + pl2Non  + DPplNon
+StEwrtNSR <- ((sg2Non + sg2Adj)*(0.82*0.58)) + DPplAdj +sg1Non + pl13Non + pl2Non  + DPplNon
 StEwrtZero <- sg2Non + sg2Adj + sg3Adj + DPsgAdj + sg3Non + DPsgNon
 ZeroWrtNSR <- sg2AdjV + sg2NonV + sg3Adj +DPsgAdj + DPplAdj + sg1Non + sg3Non + pl13Non + pl2Non +  DPsgNon + DPplNon
 ZeroWrtStE <- sg2AdjV + sg2NonV + sg3Adj + DPsgAdj + sg3Non + DPsgNon 
